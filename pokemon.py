89220e3d4e021a44312713b5af7c28ccbcebcd32
MPBaptista <miguelbaptista1000@gmail.com>
2020-06-20 04:11:08 +0100

    further implement world cration and Ash positioning

--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,4 @@
+import time
 
 class Ash():
     """
@@ -9,7 +10,7 @@ class Ash():
         Create ash at default position (0,0)
         """
         self.position = [0,0]
-        self.pokemons = 0
+        self.pokemons = 1
 
     def get_position(self):
         """ 
@@ -21,58 +22,78 @@ class Ash():
         Return current Ash position
         """
         return (self.position[0], self.position[1])
-    def move_N(self):
-        """ 
-        Move Ash North (Norte)
+    def get_pokemons(self):
         """
-        self.position[1] += 1
-    def move_S(self):
-        """ 
-        Move Ash South (Sul)
+        Get number of pokemons in Ash's Pokedex
         """
-        self.position[1] -= 1
-    def move_E(self):
-        """ 
-        Move Ash East (Este)
+        return self.pokemons
+    def add_pokemon(self):
         """
-        self.position[0] += 1
-    def move_O(self):
-        """ 
-        Move Ash West (Oeste)
+        Add new pokemon to Ash's Pokedex
         """
-        self.position[0] -= 1
+        self.pokemons += 1
+    def move(self,direction):
+        """
+        Move ash in argument direction
+        """
+        if direction == "N":
+            self.position[1] += 1
+        if direction == "S":
+            self.position[1] -= 1
+        if direction == "E":
+            self.position[0] += 1
+        if direction == "O":
+            self.position[0] -= 1
+        return (self.position[0], self.position[1])
+    def __str__(self):
+        return 'Ash está em ' + str(self.get_position_tuple()) + ' e tem ' + str(self.get_pokemons()) + ' pokemons.'
 
 class WorldMap():
     """
     Create initial map instance.
     """
     def __init__(self):
-        self.world_map = []
+        self.world_map = [(0,0)]
     def checkForPokemon(self, position):
         if position not in self.world_map:
             self.world_map.append(position)
             return True
         else:
             return False
-    
+    def __str__(self):
+        return 'Casas exploradas: ' + str(self.world_map)
 
 def main():
     ash = Ash()
     world_map = WorldMap()
-    print("Bem vindo Ash")
+    print("Bem-vindo Ash\n")
     while True:
-        sequence = (str(input("Introduz a sequência de movimentos desejada(N/S/E/O separados por vírgulas): "))).split(",")
+        sequence = str(input("Introduz a sequência de movimentos desejada(N/S/E/O): "))
+        sequence_list = []
         for element in sequence:
             if element in ["N", "S", "E", "O"]:
                 sequence_list.append(element)
             else:
                 print("Sequência inválida")
-        
-        flag = str(input("Deseja Continuar? (s/n) "))
+                break
+        start = time.process_time()
+        for direction in sequence_list:
+            if world_map.checkForPokemon(ash.move(direction)):
+                ash.add_pokemon()
+            else:
+                pass
+        end = time.process_time()
+        print(ash.get_pokemons())
+        #print(end-start)
+            
+        flag = str(input("\nDeseja Continuar? (s/n) "))
         if flag == "s":
             continue
         elif flag == "n":
+            print("\nAdeus Ash")
             break
-
+        else:
+            print("Comando inválido\n")
 if __name__ == "__main__":
     main()
+
